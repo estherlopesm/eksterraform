9f15c01d02db2891eb0be66c8a5dde4a65babd25
emacedo <root@emacedo>
2024-07-23 19:19:35 +0000

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,13 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.59.0"
     }
+  }
+  backend "s3" {
+    bucket = "esthers3"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-	region = "us-east-1"
-  # Configuration options
+  region = "us-east-1"
 }
+
